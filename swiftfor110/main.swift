90e169e25a164fccf221723399b86ffa30c8588c
huangchengyi <735420131@qq.com>
2018-10-12 20:12:12 +0800

    Initial Commit

---
+++ swiftfor110/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftfor110
+//
+//  Created by s20171106185 on 2018/10/12.
+//  Copyright © 2018年 s20171106185. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
